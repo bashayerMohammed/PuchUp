a9575d187615d919dd33e98c16f017470e9f82ca
bashayer <Bashayer.65@gmail.com>
2016-02-02 13:23:08 +0300

    Add Class with Modified

--- app/src/main/java/ksu/puchup/MainActivity.java
+++ app/src/main/java/ksu/puchup/MainActivity.java
@@ -52,6 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void testUpload() {
         int i = 1;
-
+        int q = 2;
     }
 }
---
+++ app/src/main/java/ksu/puchup/TestingClass.java
@@ -0,0 +1,7 @@
+package ksu.puchup;
+
+/**
+ * Created by bashayer on 02/02/2016.
+ */
+public class TestingClass {
+}
